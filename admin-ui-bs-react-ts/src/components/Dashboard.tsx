8e2fe48225f524c115d105fe9827fecba5598a8d
Sunny Kumar <rounsunn@gmail.com>
2023-07-21 01:48:42 +0530

    Learnings after completing milestone 1
    React rendering process:
    Page is rendered initially with whatever data it has, then immediately useEffect componentDidMount lifecycle runs and State gets it value so till the useEffect is loading data, we can use a state isLoading to display
    In the block useEffect - state is not updated yet, try console the state just after updating it in useEffect.
    
    Data Fetching:
    Inbuilt browser api: fetch, used it with async/await and response.json()
    
    TypeScript Learnings:
    If we assign value with known type to variable with same type then its ok. While declaring need to tell the type and while assigning assigned value type should be told.
    Made Interface for response and while declaring, assigned empty of that type to state, while receiving as userList[]
    
    interface usersList {
      id: string;
      name: string;
      email: string;
      role: string;
    }
    
    const defaultUsers: usersList[] = [];
    
    const [users, setUsers] = useState(defaultUsers);
    
    const data = (await response.json()) as usersList[];
    
    setUsers(data); console.log(users);
    
    void getUsersList();
    
    We can click on eslint error in terminal to open official eslint docs to know about error and fix them
    Like for promise returned api calls, when calling async fn call, we need to do void promiseCall().
    Why ? “a good way to explicitly mark a promise as intentionally not awaited.”

--- admin-ui-bs-react-ts/src/components/Dashboard.tsx
+++ admin-ui-bs-react-ts/src/components/Dashboard.tsx
@@ -12,21 +12,40 @@ const defaultUsers: usersList[] = [];
 const Dashboard = () => {
   const [users, setUsers] = useState(defaultUsers);
 
+  const showUsers = () => {
+    console.log("Show users called: ", users);
+    return users.map((user) => (
+      <div key={user.id}>
+        <>{user.id}</>
+        <>{user.name}</>
+        <>{user.email}</>
+        <>{user.role}</>
+      </div>
+    ));
+  };
+
   useEffect(() => {
     console.log("fetchUsersList is called");
     const config = {
       method: "get",
       url: "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json",
     };
-    fetch(config.url)
-      .then((response) => {
-        console.log(response.json());
-        // return response.json();
-      })
-      .catch((err) => console.log(err));
+    const getUsersList = async () => {
+      try {
+        const response = await fetch(config.url);
+        console.log(response);
+        const data = (await response.json()) as usersList[];
+        console.log(data);
+        setUsers(data);
+        // console.log(users);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    void getUsersList();
   }, []);
 
-  return <div>{"users"}</div>;
+  return <div>{showUsers()}</div>;
 };
 
 export default Dashboard;
