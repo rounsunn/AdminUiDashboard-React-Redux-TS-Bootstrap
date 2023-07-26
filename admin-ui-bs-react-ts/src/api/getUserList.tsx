e27c0b915487b18a63b4f8ab086e810199f6acbf
Sunny Kumar <rounsunn@gmail.com>
2023-07-26 19:59:15 +0530

    Milestone: 3 - Search Feature /////////////////////////////////////////////////////////////////////// Interface annotation while receiving and setting response.data interface UserInterface {   id: string;   name: string;   email: string;   role: string; }
    interface UserListInterface {
      users: UserInterface[];
    }
    
    const defaultUsers: UserListInterface = { users: [] }; - default type
    const [USERS, setUSERS] = useState(defaultUsers); - Intialize
    const response = await getInstance<UserInterface[]>(""); -- setting
    setUSERS({ users: response.data });
    
    <DisplayTable users={filteredUsers.users} /> - passing
    const DisplayTable = ({ users }: UserListInterface) -- receiving
    
    cannot receive as DisplayTable = (users: UserInterface[])
    that's why an extra users field in USERS and
    All the care while setting and receiving
    
    ///////////////////////////////////////////////////////////////////////
    handleSeacrh - onChange event annotation
    const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {}
    if (e.target instanceof HTMLInputElement) searchText = e.target.value;
    filter and sett
    Setting filteredSearch firt time in getUsers
    
    ///////////////////////////////////////////////////////////////////////
    Best way to do above
    Implementing getUsers function having api call
    Search Feature

--- admin-ui-bs-react-ts/src/api/getUserList.tsx
+++ admin-ui-bs-react-ts/src/api/getUserList.tsx
@@ -1,21 +1,24 @@
 import getInstance from "./api";
-import { UserListInterface } from "../interface/userInterface";
+import { UserInterface, UserListInterface } from "../interface/userInterface";
 
 interface setProps {
-  setUsers: React.Dispatch<React.SetStateAction<UserListInterface>>;
+  setUSERS: React.Dispatch<React.SetStateAction<UserListInterface>>;
+  setFilteredUsers: React.Dispatch<React.SetStateAction<UserListInterface>>;
   setErrorMessage: React.Dispatch<React.SetStateAction<string>>;
   setIsloading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const getUserList = async ({
-  setUsers,
+  setUSERS,
+  setFilteredUsers,
   setErrorMessage,
   setIsloading,
 }: setProps) => {
   console.log("gteuserList is called");
   try {
-    const response = await getInstance<UserListInterface>("");
-    setUsers(response.data);
+    const response = await getInstance<UserInterface[]>("");
+    setUSERS({ users: response.data });
+    setFilteredUsers({ users: response.data });
   } catch (error) {
     console.log(error);
     if (error instanceof Error) {
--- admin-ui-bs-react-ts/src/components/Dashboard.tsx
+++ admin-ui-bs-react-ts/src/components/Dashboard.tsx
@@ -6,17 +6,48 @@ import DisplayTable from "./DisplayTable";
 const defaultUsers: UserListInterface = { users: [] };
 
 const Dashboard = () => {
-  const [users, setUsers] = useState(defaultUsers);
+  const [USERS, setUSERS] = useState(defaultUsers);
+  const [filteredUsers, setFilteredUsers] = useState(defaultUsers);
   const [isLoading, setIsloading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
 
+  const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e);
+    console.log("handleSearch");
+    let searchText: string;
+    if (e.target instanceof HTMLInputElement) searchText = e.target.value;
+    const filteredUSERS = USERS.users.filter((user) => {
+      return (
+        user.name.includes(searchText) ||
+        user.email.includes(searchText) ||
+        user.role.includes(searchText)
+      );
+    });
+    setFilteredUsers({ users: filteredUSERS });
+  };
+
   useEffect(() => {
-    void getUserList({ setUsers, setErrorMessage, setIsloading });
+    void getUserList({
+      setUSERS,
+      setFilteredUsers,
+      setErrorMessage,
+      setIsloading,
+    });
   }, []);
 
   return (
     (isLoading && "Loading Data") ||
-    (errorMessage !== "" && errorMessage) || <DisplayTable users={users} />
+    (errorMessage !== "" && errorMessage) || (
+      <div>
+        <input
+          type="text"
+          className="input-group"
+          placeholder="search"
+          onChange={handleSearch}
+        />
+        <DisplayTable users={filteredUsers.users} />
+      </div>
+    )
   );
 };
 
